8d9783cba6b0c208be241c1316e8d1c7503a4238
Tizzio <adriano.tummi@hotmail.it>
2014-08-07 21:48:55 +0200

    first release

---
+++ js/main.js
@@ -0,0 +1,530 @@
+var editor = null; 
+ 
+/*
+window.onerror = function(msg, url, linenumber) {
+	return true;
+}
+*/
+function StartEditor(){ 
+	editor = new Editor();
+	editor.Draw();
+	
+		
+	editor.canvas.addEventListener("mousemove", function(s) {
+		editor.mouseX = Math.round(s.pageX - editor.ctx.canvas.offsetLeft + editor.div.scrollLeft);
+		editor.mouseY = Math.round(s.pageY - editor.ctx.canvas.offsetTop + editor.div.scrollTop);
+		if(editor.mouseLeft){  
+		}
+		else if(editor.mouseRight){  
+		}
+	}, false);
+
+	editor.canvas.addEventListener("mousedown", function(e) { 
+		switch (e.which) {
+		case 1: 
+			editor.mouseLeft = true;
+			editor.LeftClick();
+			break; 
+		case 3:  
+			editor.mouseRight = true;
+			editor.RightClick();
+			break;
+		}
+	}, false);
+
+	
+	
+	window.addEventListener("mouseup", function(e) { 
+		switch (e.which) {
+		case 1: 
+			editor.mouseLeft = false;			
+			editor.LeftRelease();
+			break; 
+		case 3:  
+			editor.mouseRight = false;		
+			editor.RightRelease();
+			break;
+		}
+	}, false);
+	
+	
+	
+	editor.canvas.addEventListener("mousewheel", function(f) {
+		if(f.wheelDelta > 0){
+			editor.scaling += f.wheelDelta/100;
+			$("scaling").value = editor.scaling; 
+		}else if(f.wheelDelta < 0){
+			editor.scaling +=  f.wheelDelta/100;
+				
+			if(editor.scaling < 0.025){
+				editor.scaling = 0.025;
+			}
+			
+			$("scaling").value = editor.scaling; 
+		}
+		
+		editor.Draw();
+   }, false);
+
+	
+	
+}
+
+function Editor(){  
+	//working area 
+	
+	this.canvas = $("EditorCanvas");
+	
+	this.SetSize = function(){
+		this.areaW = ((window.innerWidth-5)/4*3 - 5);
+		this.areaH = (window.innerHeight-5);
+		
+		this.centerX = this.areaW / 2;
+		this.centerY = this.areaH / 2;
+		
+		this.div = $("EditorDiv");
+		this.div.padding = "0px";
+		this.div.style.width = this.areaW + "px";
+		this.div.style.height = this.areaH + "px";
+		 
+		this.canvas.setAttribute("width", this.areaW);
+		this.canvas.setAttribute("height",this.areaH-5);
+		
+		optWidth = (window.innerWidth-5)/4 ;
+		this.divOpt = $("Options");
+		this.divOpt.style.padding = "5px";
+		this.divOpt.style.width = optWidth -10 + "px";
+		this.divOpt.style.height = (window.innerHeight-5 - 8) + "px"; 
+		this.divOpt.style.left = window.innerWidth - optWidth - 6 +"px" 
+		
+		$("output").style.width = (optWidth -25)  + "px";
+		
+		if(this.Draw) this.Draw();
+	}
+	this.SetSize();
+	
+	
+	this.canvas.addEventListener('contextmenu', function (event) {
+		event.preventDefault();
+	});
+	
+	this.canvas.addEventListener("mousedown", function(e){
+		if(e.which == 1){
+			editor.mouseLeft = true; 
+		}
+		else if(e.which == 3){  
+			editor.mouseRight = true; 
+		}
+		
+	});
+	
+	this.ctx = this.canvas.getContext("2d");
+	 
+	this.scaling = 1;
+	
+	this.viewX = 0;
+	this.viewY = 0;
+	 
+	this.mouseX = 0;
+	this.mouseY = 0;
+	
+	this.mouseXPrev = 0;
+	this.mouseYPrev = 0;
+	
+	this.viewDragged = false;
+	
+	this.mouseLeft = false;
+	this.mouseRight = false;
+	
+	this.mode = 0; 
+	
+	this.verts = [];
+	
+	this.dragVertex = undefined;
+	
+	this.undoList = [[]]; 
+	this.undoIndex = 0;
+	 
+	$("mode").onchange = function(){
+		editor.mode = this.selectedIndex; 
+	};
+	
+	this.SetMode = function(mode){
+		this.mode = mode;
+		$("mode").selectedIndex = mode;
+	}
+	 
+	$("export").onclick = function(){
+		editor.Export();
+	};
+	
+	$("loadMesh").onclick = function(){
+		editor.LoadMesh();
+	};
+	
+	$("newMesh").onclick = function(){
+		if(editor.verts.length > 0){
+			var w = prompt("Current mesh will be lost.\nCreate new mesh? ","Yes");
+			if (w == "Yes"){
+				editor.verts = [];
+				editor.Draw();
+			}
+		}
+	};
+	
+	
+	$("scaling+").onclick = function(){
+		editor.scaling *= 2; 
+		$("scaling").value = editor.scaling;
+		editor.Draw();
+	};
+	
+	
+	$("scaling-").onclick = function(){
+		if(editor.scaling > 0.01){
+			editor.scaling /= 2;
+			$("scaling").value = editor.scaling;
+			editor.Draw();
+		}
+	};
+	
+	$("FunReverse").onclick = function(){
+		editor.verts.reverse();
+	}
+	
+	this.Export = function(){ 
+		$("output").value = JSON.stringify(this.verts);
+	}
+	
+	this.LoadMesh = function(){ 
+		this.verts = JSON.parse($("output").value);
+		editor.Draw();
+	}
+	
+	  
+	
+	//Load image file
+	var finput = $("loadFile");
+	finput.onchange = function(){
+		var file = this.files[0];
+		var fr = new FileReader();
+		fr.onload = function(){
+			//this.i = new Image();
+			editor.img.src = this.result;
+			$("srcImg").src = this.result;
+			editor.Draw();
+		};
+		fr.readAsDataURL(file);
+	} 
+	 
+	  
+	//Load Resources
+	rh = new ResourcesHandler( function(){
+		this.loaded = true; 
+		editor.Draw();
+	});
+	  
+	this.img = rh.LoadSprite("res/img.png", 1); 
+	
+	this.imgDot = rh.LoadSprite("res/dot.png", 1); 
+	 
+	 
+	 
+	 
+	this.Draw = function(){ 
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.ctx.globalAlpha = 0.5;
+		this.ctx.save();
+		this.ctx.translate(this.centerX, this.centerY);
+		this.ctx.scale(this.scaling, this.scaling);
+		this.ctx.drawImage(this.img, - this.img.width / 2,  - this.img.height / 2);
+		this.ctx.restore();
+		
+		this.ctx.globalAlpha = 1;
+		
+		
+		var len = this.verts.length;
+		
+		if(len > 0){
+			this.ctx.save();
+			this.ctx.translate(this.centerX, this.centerY);
+			this.ctx.beginPath();
+			var v = this.verts[0];
+			this.ctx.moveTo(v[0] * this.scaling, v[1] * this.scaling);
+			for(var i=0; i<len; i++){
+				var v = this.verts[i];
+				this.ctx.lineTo(v[0] * this.scaling, v[1] * this.scaling);
+				this.ctx.drawImage(this.imgDot, v[0] * this.scaling  - 4,  v[1] * this.scaling - 4);
+			}
+			//this.ctx.closePath();
+			this.ctx.stroke();
+			this.ctx.restore();
+		}
+		
+	}
+	 
+	this.Drag = function(v){
+		this.dragVertex = v;
+		window.requestAnimFrame( function(){
+			editor.Update();
+		});
+	}
+	
+	this.DragView = function(v){
+		this.viewDragged = true;
+		this.mouseXPrev = this.mouseX;
+		this.mouseYPrev = this.mouseY;
+		
+		window.requestAnimFrame( function(){
+			editor.Update();
+		});
+	}
+	
+	this.LeftClick = function(){ 
+		switch(this.mode){
+			case 0: //move
+				var v = this.GetNearestVert();
+				if(v !== null){
+					this.Drag(v);
+				}else{
+					this.DragView();
+				}
+				break;
+			
+			case 1: //add
+				var v = [(this.mouseX - this.centerX) / this.scaling, (this.mouseY - this.centerY )  / this.scaling];
+				this.Drag(this.verts.length);
+				this.verts.push(v); 
+				break;
+				
+			case 2: //delete
+				var v = this.GetNearestVert();
+				if(v !== null){
+					this.verts.splice(v, 1);
+					this.AddUndo();
+				}else{
+					this.DragView();
+				}
+				break;
+			
+			
+		}
+		this.Draw();
+	}
+	
+	this.LeftRelease = function(){
+		if(this.dragVertex != undefined) 
+				this.AddUndo();
+		this.dragVertex = undefined;
+		this.viewDragged = false;
+	}
+	
+	this.RightRelease = function(){
+		if(this.dragVertex != undefined) 
+				this.AddUndo();
+		this.dragVertex = undefined;
+		this.viewDragged = false;
+	}
+	
+	this.RightClick  = function(){
+		switch(this.mode){
+			case 0: 
+				break;
+			
+			case 1: //add segment
+				var index =  this.GetNearestSegment(30)
+				if(index != null){
+					var v = [(this.mouseX - this.centerX) / this.scaling, (this.mouseY - this.centerY )  / this.scaling];
+					
+					console.log(this.verts);
+					//array insert
+					var temp = this.verts.slice(0, index+1);
+					temp.push(v);
+					this.verts = temp.concat(this.verts.slice(index+1, this.verts.length));
+
+					this.Drag(index+1); 
+				} 
+				break;
+				
+			case 2: 
+				break; 
+			
+		}
+		this.Draw();
+	}
+	
+	this.Update = function(){
+		if(this.dragVertex != undefined){
+			var v = this.verts[this.dragVertex];
+			v[0] = parseInt((this.mouseX - this.centerX) / this.scaling);
+			v[1] = parseInt((this.mouseY - this.centerY) / this.scaling);
+			this.Draw();
+			window.requestAnimFrame( function(){
+				editor.Update();
+			});
+		}
+		
+		if(this.viewDragged){
+			this.centerX += this.mouseX - this.mouseXPrev;
+			this.centerY += this.mouseY - this.mouseYPrev;
+			this.mouseXPrev = this.mouseX;
+			this.mouseYPrev = this.mouseY;
+			this.Draw(); 
+			 
+			window.requestAnimFrame( function(){
+				editor.Update();
+			});
+		}
+	}
+	
+	 
+	
+	
+	this.GetNearestVert = function(){
+		for(var i=0; i<this.verts.length; i++){
+			var v = this.verts[i];
+			if(distance(v[0]*this.scaling + this.centerX, v[1]*this.scaling + this.centerY, this.mouseX, this.mouseY) < 6){
+				return i;
+			}
+		}
+		return null;
+	}
+	
+	this.GetNearestSegment = function(maxDist){
+		var len = this.verts.length;
+		
+		if(len < 2)
+			return null;
+			
+		var segment = null;
+		var minDist = Infinity;
+		for(var i=0; i<len-1; i++){
+			var v0 = this.verts[i];
+			var v1 = this.verts[i+1];
+			var segdist = segmentDistance(
+				v0[0]*this.scaling+this.centerX, v0[1]*this.scaling+this.centerY,
+				v1[0]*this.scaling+this.centerX, v1[1]*this.scaling+this.centerY,
+				this.mouseX,
+				this.mouseY);
+			if(segdist < minDist && segdist < maxDist){
+				segment = i;
+				minDist = segdist;
+			}
+		}
+		return segment;
+	
+	}
+	
+	this.ResetEditor = function(){
+	}
+
+	this.Undo = function(){ 
+	
+		if(this.undoIndex > 0){
+			this.undoIndex--;
+			this.verts = this.undoList[this.undoIndex];
+			this.Draw();
+		}
+		console.log(this.undoIndex, this.undoList);
+	}
+	
+	this.Redo = function(){
+		if(this.undoIndex < this.undoList.length-1){
+			this.undoIndex++;
+			this.verts = this.undoList[this.undoIndex];
+			this.Draw();
+		}
+		console.log(this.undoIndex, this.undoList);
+		
+	}
+	
+	this.AddUndo = function(){
+		console.log(this.undoList.splice(this.undoIndex+1, this.undoList.length));
+		this.undoIndex++;
+		console.log("add undo: "+this.undoIndex, this.undoList);
+		this.undoList.push(this.verts.slice());
+	}
+	
+}
+  
+ 
+window.addEventListener("keydown", function(e) { 
+	switch(e.keyCode){
+		case 49: //1
+			editor.SetMode(0);
+			break;
+			
+		case 50: //2
+			editor.SetMode(1);
+			break;
+			
+		case 51: //3
+			editor.SetMode(2);
+			break;
+			
+		
+		case 90: //Z
+			editor.Undo();
+			break;
+			
+			
+		case 89: //Y
+			editor.Redo();
+			break;
+		 
+	}
+}, false);
+
+
+const TO_DEG = 180 / Math.PI;
+const TO_RAD = Math.PI / 180;
+const PI2 = Math.PI / 2;
+
+function distance(x1, y1, x2, y2){ 
+	return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+}
+
+function directionDeg(x1, y1, x2, y2) {
+   return (Math.atan2(y1 - y2, x2 - x1) * TO_DEG + 360 ) % 360;
+}
+
+function direction(x1, y1, x2, y2) {
+   return Math.atan2(y1 - y2, x2 - x1);
+}
+
+
+function rectDistance(x1, y1, x2, y2, xp, yp){
+	var dist = Math.sqrt((y2-y1)*(y2-y1) + (x1-x2)*(x1-x2));  
+	return Math.abs(((y2-y1)*(x1-xp) + (x1-x2)*(y1-yp)) / dist);
+}
+
+function rectSign(x1, y1, x2, y2, xp, yp){   
+	var val = (y2-y1)*(x1-xp) + (x1-x2)*(y1-yp);
+	if(val > 0) return 1;
+	else if(val < 0) return -1;
+	return 0;
+}
+ 
+function segmentDistance(x1, y1, x2, y2, xp, yp){
+	
+	var dir = Math.atan2(y1 - y2, x2 - x1);
+	var cos1 = Math.cos(dir + PI2);
+	var sin1 = -Math.sin(dir + PI2);
+	var cos2 = Math.cos(dir - PI2);
+	var sin2 = -Math.sin(dir - PI2);
+	
+	//parallel rects
+	var sign1 = rectSign(x1+cos1, y1+sin1, x1+cos2, y1+sin2, xp, yp);
+	var sign2 = rectSign(x2+cos1, y2+sin1, x2+cos2, y2+sin2, xp, yp);
+	
+	//if point is beetween 2 rects
+	if(sign1 != sign2)
+		return rectDistance(x1, y1, x2, y2, xp, yp);
+		
+	return Infinity;
+}
+
+
+
+
+
